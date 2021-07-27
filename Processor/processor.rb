b8959838ca540a4f4fcb35420d272c94f5a4a3b3
EC2 Default User <ec2-user@ip-172-31-81-231.ec2.internal>
2021-07-27 14:41:11 +0000

    Faz updates. First in ages..

--- Processor/processor.rb
+++ Processor/processor.rb
@@ -10,22 +10,13 @@ require 'net/http'
 require 'crack/xml'
 
 ### Start thread to read messages off the bus and act accordingly.
-bunny = Bunny.new
-bunny.start
-@channel = bunny.create_channel
-queue = @channel.queue("#{ENV['FAZ_QUEUE_NAME']}_received")
-
-@mongo = Mongo::Client.new('mongodb://127.0.0.1:27017/fazool')
-@collection = @mongo[:quotes]
-
-@routing_key = "send_to_#{ENV['FAZ_QUEUE_NAME']}"
-
 COMMANDS = {
   'help'               => "Display this help text",
   'weather <location>' => 'Check weather',
   'who <text>'         => 'Who dunnit?',
   'what <text>'        => "Arbitrary questions around 'what' i.e. 'What gives?'",
   'what time is it'    => "Self explanatory",
+  'how <adjective> are we?' => 'Current US covid summaries',
   'how <text>'         => "How questions: How is the sky blue?",
   'why <text>'         => "Why questions",
   'when <text>'        => "When questions i.e. when will i get married?",
@@ -66,11 +57,9 @@ def robinhood_command(prefix)
   end
 end
 
-# def covid_command(prefix, region, location='https://opendata.arcgis.com/datasets/1cb306b5331945548745a5ccd290188e_1.geojson', limit=10)
 def covid_command(prefix, region, location='https://prod-hub-indexer.s3.amazonaws.com/files/1cb306b5331945548745a5ccd290188e/1/full/4326/1cb306b5331945548745a5ccd290188e_1_full_4326.geojson', limit=10)
   if region and region =~ /^usa$/i
     region.upcase!
-    # location = 'https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson'
     location = 'https://prod-hub-indexer.s3.amazonaws.com/files/628578697fb24d8ea4c32fa0c5ae1843/0/full/4326/628578697fb24d8ea4c32fa0c5ae1843_0_full_4326.geojson'
   end
   url = URI.parse(location)
@@ -78,34 +67,40 @@ def covid_command(prefix, region, location='https://prod-hub-indexer.s3.amazonaw
   res = Net::HTTP.start(url.hostname, url.port, use_ssl: true) {|http|
     http.request(req)
   }
-  case res 
-  when Net::HTTPSuccess     then res 
+  case res
+  when Net::HTTPSuccess     then res
   when Net::HTTPRedirection then covid_command(res['location'], limit - 1)
   else
     response.error!
-  end 
+  end
   parsed = JSON.parse(res.body)
-  death_count = 0 
-  case_count = 0 
-  recovery_count = 0 
-   puts " REGION: '#{region}'"
+  death_count = 0
+  case_count = 0
+  active_count = 0
+  recovery_count = 0
   if region != '' and region != 'USA'
     region_data = parsed['features'].select{|x| x['properties']['Province_State'] =~ /#{region}/i}
     if region_data.count == 0 # try Country Region
       region_data = parsed['features'].select{|x| x['properties']['Country_Region'] =~ /#{region}/i}
     end
-    region_data.map{|x| death_count += x['properties']['Deaths']}
-    region_data.map{|x| recovery_count += x['properties']['Recovered']}
-    region_data.map{|x| case_count += x['properties']['Confirmed']}
+    region_data.map{|x| death_count += x['properties']['Deaths'] || 0}
+    region_data.map{|x| recovery_count += x['properties']['Recovered'] || 0}
+    region_data.map{|x| active_count += x['properties']['Active'] || 0}
+    region_data.map{|x| case_count += x['properties']['Confirmed'] || 0}
   else
-    parsed['features'].map{|x| death_count += x['properties']['Deaths']}
-    parsed['features'].map{|x| case_count += x['properties']['Confirmed']}
-    parsed['features'].map{|x| recovery_count += x['properties']['Recovered']}
-  end 
+    parsed['features'].map{|x| death_count += x['properties']['Deaths'] || 0}
+    parsed['features'].map{|x| case_count += x['properties']['Confirmed'] || 0}
+    parsed['features'].map{|x| active_count += x['properties']['Active'] || 0}
+    parsed['features'].map{|x| recovery_count += x['properties']['Recovered'] || 0}
+  end
   death_count = death_count.to_s.reverse.scan(/.{1,3}/).join(',').reverse
   case_count = case_count.to_s.reverse.scan(/.{1,3}/).join(',').reverse
+  active_count = active_count.to_s.reverse.scan(/.{1,3}/).join(',').reverse
   recovery_count = recovery_count.to_s.reverse.scan(/.{1,3}/).join(',').reverse
-  push_message("#{prefix} COVID19 #{region == '' ?  "World" : region.capitalize} - Dth: #{death_count}, Case: #{case_count}, Rcvr: #{recovery_count} (#{Time.now.to_s.split(" ")[0..1].join(' ')[0..15]})")
+  push_message("#{prefix} COVID19 #{region == '' ?  "World" : region.capitalize} - Active: #{active_count},  Dth: #{death_count}, Case: #{case_count}, Rcvr: #{recovery_count} (#{Time.now.to_s.split(" ")[0..1].join(' ')[0..15]})")
+  parsed[:created_at] = Time.now
+  parsed[:region] = region
+  @covid_collection.insert_one(parsed)
 end
 
 
@@ -128,10 +123,7 @@ end
 
 def weather_command(prefix, locale)
   locale.chomp!
-  puts "Locale requested: #{locale}"
-  # sed 's/\x1b\[[0-9;]*m//g'
   weather = `curl -B http://wttr.in/#{locale}?T 2>/dev/null | head -7`
-  puts " Weather response: #{weather}"
   push_message("#{prefix} : #{weather}")
   weather.split("\n").each do |x|
     push_message("#{prefix} : #{x.gsub(' ', '%b').gsub('B0', '').gsub(/\\/, '%\\')}")
@@ -142,8 +134,6 @@ def recall_command(prefix, command, actor, with_count=false)
   query = { quote: nil }
   id_str = ""
   id_match = /( with id)(?:$|\r|\n)/.match(command)
-  puts " id_match: #{id_match}"
-  puts " command: #{command}"
   if id_match
     id_str = id_match[1]
     command.gsub!(/ with id(?:$|\r|\n)/, '')
@@ -174,9 +164,7 @@ def recall_command(prefix, command, actor, with_count=false)
       end
     end
     if result_array.count > 0
-  puts " RESULT ARRAY: #{result_array}"
       result_array.sort{|a,b| b[1] <=> a[1]}.each do |x|
-puts " X: #{x}"
         push_message("#{prefix} #{x[0]}")
       end
     else
@@ -219,7 +207,6 @@ puts " X: #{x}"
     # Don't need to make the find query if we just want a count.
     quote = @collection.find(query).limit(-1).skip(rand(query_count)).first
   end
-  puts "  QUOTE: #{quote.inspect}"
 
   if quote or with_count
     if id_str.length > 1
@@ -286,16 +273,15 @@ def will_command(prefix)
   push_message("#{prefix} #{phrase_array.sample}")
 end
 
-
-def how_command(prefix)
+def how_command(prefix, command=nil)
   phrase_array = [
     'By osmosis.',
     'By removing his head from his.. hey, whoah, I just saw a trail.',
     'North by northweset.',
     "By visiting your grandma's place.",
-    "Elementary, Watson.", 'How should I know?', 
-    'I have no idea.', 
-    'Just follow the instructions.', 
+    "Elementary, Watson.", 'How should I know?',
+    'I have no idea.',
+    'Just follow the instructions.',
     'Let me google that for you.',
     'Let me Bing that for ya...'
   ]
@@ -307,7 +293,7 @@ def when_command(prefix, actor)
   random = @collection.find(:quote => /"[wW]hen /).limit(-1).skip(rand(count)).first
   if random
     push_message("#{prefix} #{/"(.*?)"/.match(random['quote'])[1]}")
-  else 
+  else
     push_message("#{prefix} I'm sorry. I have no answer for that, #{actor}")
   end
 end
@@ -325,7 +311,7 @@ def what_command(prefix, command, actor)
     # push_message("#{prefix} #{phrase_array.sample} '#{thing}', #{actor}")
     if random
       push_message("#{prefix} #{/"(.*?)"/.match(random['quote'])[1]}")
-    else 
+    else
       push_message("#{prefix} I'm sorry. I have no answer for that, #{actor}")
     end
   end
@@ -337,7 +323,7 @@ def why_command(prefix, actor)
   random = @collection.find(:quote => /"[bB]ecause/).limit(-1).skip(rand(count)).first
   if random
     push_message("#{prefix} #{/"(.*?)"/.match(random['quote'])[1]}")
-  else 
+  else
     push_message("#{prefix} I'm sorry. I have no answer for that, #{actor}")
   end
 end
@@ -395,7 +381,7 @@ def command_logic(command, page_bool, actor)
     # base_command = /^(.*?)[ "]/.match(command)[1]
     if command =~ / /
       base_command = /^(.*?) /.match(command)
-    else 
+    else
       base_command = /^(\w+)/.match(command)
     end
     if base_command
@@ -426,7 +412,7 @@ def command_logic(command, page_bool, actor)
   when /^[wW]hat/
     what_command(prefix, command, actor)
   when /^[hH]ow/
-    how_command(prefix)
+    how_command(prefix, command)
   when /^[wW]hy/
     why_command(prefix, actor)
   when /^[wW]hen/
@@ -454,76 +440,90 @@ def command_logic(command, page_bool, actor)
   end
 end
 
+def main_loop
+  begin
+    bunny = Bunny.new
+    bunny.start
+    @channel = bunny.create_channel
+    queue = @channel.queue("#{ENV['FAZ_QUEUE_NAME']}_received")
 
-begin
-  puts "Subscribing to queue '#{ENV['FAZ_QUEUE_NAME']}-received'..."
-  queue.subscribe(:block => true) do |delivery_info, properties, body|
-    puts " Got body from bus: #{body}"
-    # Body will either be a request for data recall
-    #   or stuff that needs to be filtered/recorded in the DB
-    if body =~ /"Faz(...)?,/ or body =~ / to you\./ or body =~ / pages: /
-      # We have a command.
-      page_type = "MUCK"
-      if body =~ / to you\./
-        is_page = true
-      elsif body =~ / pages: /
-        is_page = true
-        page_type = "MUSH"
-      end
-      request = nil
-      if is_page
-        if page_type == "MUCK"
-          request = /"(.*?)"/.match(body)[1]
-        else
-          request = /pages: (.*?)$/.match(body)[1]
+    # @mongo = Mongo::Client.new('mongodb://127.0.0.1:27017/fazool')
+    @mongo = Mongo::Client.new([ '127.0.0.1:27017' ],
+                               user: 'fazool',
+                               password: ENV['FAZ_PASS'],
+                               database: 'fazool' )
+
+    @collection = @mongo[:quotes]
+    @covid_collection = @mongo[:covid]
+    @routing_key = "send_to_#{ENV['FAZ_QUEUE_NAME']}"
+
+    puts "Subscribing to queue '#{ENV['FAZ_QUEUE_NAME']}-received'..."
+    queue.subscribe(:block => true) do |delivery_info, properties, body|
+      puts " Got body from bus: #{body}"
+      # Body will either be a request for data recall
+      #   or stuff that needs to be filtered/recorded in the DB
+      if body =~ /"Faz(...)?,/ or body =~ / to you\./ or body =~ / pages: /
+        # We have a command.
+        page_type = "MUCK"
+        if body =~ / to you\./
+          is_page = true
+        elsif body =~ / pages: /
+          is_page = true
+          page_type = "MUSH"
         end
+        request = nil
+          if is_page
+          if page_type == "MUCK"
+            request = /"(.*?)"/.match(body)[1]
+            else
+            request = /pages: (.*?)$/.match(body)[1]
+          end
+        else
+          request = /"Faz(?:...)?, (.*?)"/.match(body)[1]
+        end
+        actor = /^\[(.*?)\(/.match(body)[1]
+        if request
+          command_logic(request, is_page, actor)
+          end
       else
-        request = /"Faz(?:...)?, (.*?)"/.match(body)[1]
-      end
-      actor = /^\[(.*?)\(/.match(body)[1]
-      if request
-        command_logic(request, is_page, actor)
-      end
-    else
-      # Record this to the database.
-      if body !~ /^##/ and body !~ /^You / and body !~ /^Fazool /
-        if body =~ /^\[[a-zA-Z0-9]/
-          real_body = body.match(/^\[.*?\](.*)/).captures[0]
-          actor = body.split(' ').shift
-          if actor.match(/^\[[a-zA-Z0-9]+\(#(\d+)\)/)
-            author_id = actor.match(/^\[[a-zA-Z0-9]+\(#(\d+)\)/).captures[0]
-            if actor =~ /<-/ # this is a force.
-              # look up author_id
-              forced_by_id = actor.match(/<-\(#(\d+)\)/).captures[0]
-              forced_by = @collection.find(author_id: forced_by_id, quote: {"$ne": nil}).first['author']
-            end
-            actor = actor.match(/^\[([a-zA-Z0-9]+)\(/).captures[0]
-            data = {author_id: author_id, author: actor, quote: real_body, created_at: Time.now, forced_by: forced_by, is_pose: false}
-            if body =~ /saypose/
-              data[:is_pose] = true
-            end
-            @collection.insert_one(data)
-            if real_body =~ /https?:/
-              url = real_body.match(/(http.*?)[ "]/)[0].to_s
-              url.gsub!(/"$/, '')
-              key = ENV['FAZ_SHORTENER_KEY']
-              shortener_login = ENV['FAZ_SHORTENER_LOGIN']
-              shortened = `curl 'http://api.bitly.com/v3/shorten\?login=#{shortener_login}&apiKey=#{key}&longUrl=#{url}&version=2.0.1' 2>/dev/null`
-              resp = JSON.parse(shortened)
-              push_message("say #{resp['data']['url']}")
+        # Record this to the database.
+        if body !~ /^##/ and body !~ /^You / and body !~ /^Fazool /
+          if body =~ /^\[[a-zA-Z0-9]/
+            real_body = body.match(/^\[.*?\](.*)/).captures[0]
+            actor = body.split(' ').shift
+            if actor.match(/^\[[a-zA-Z0-9]+\(#(\d+)\)/)
+              author_id = actor.match(/^\[[a-zA-Z0-9]+\(#(\d+)\)/).captures[0]
+              if actor =~ /<-/ # this is a force.
+                # look up author_id
+                forced_by_id = actor.match(/<-\(#(\d+)\)/).captures[0]
+                forced_by = @collection.find(author_id: forced_by_id, quote: {"$ne": nil}).first['author']
+              end
+              actor = actor.match(/^\[([a-zA-Z0-9]+)\(/).captures[0]
+              data = {author_id: author_id, author: actor, quote: real_body, created_at: Time.now, forced_by: forced_by, is_pose: false}
+              if body =~ /saypose/
+                data[:is_pose] = true
+              end
+              @collection.insert_one(data)
+              if real_body =~ /https?:/
+                url = real_body.match(/(http.*?)[ "]/)[0].to_s
+                url.gsub!(/"$/, '')
+                key = ENV['FAZ_SHORTENER_KEY']
+                shortener_login = ENV['FAZ_SHORTENER_LOGIN']
+                shortened = `curl 'http://api.bitly.com/v3/shorten\?login=#{shortener_login}&apiKey=#{key}&longUrl=#{url}&version=2.0.1' 2>/dev/null`
+                resp = JSON.parse(shortened)
+                push_message("say #{resp['data']['url']}")
+              end
             end
           end
         end
       end
     end
+  rescue => e
+    Rails.logger.info "Handling error: #{e}"
+    Rails.logger.info "Reentering main loop..."
+    sleep 1
+    main_loop
   end
-rescue Interrupt => e
-  puts " Error: #{e}"
-  bunny.close
-  exit(1)
 end
 
-
-
-
-
+main_loop
