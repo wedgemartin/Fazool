d3cc711d2db3992a6bf8e816a52492f0007f3369
Wedge Martin <wedge@shutdown.com>
2026-01-27 13:53:55 -0300

    handle network drops better

--- Connector/connector.rb
+++ Connector/connector.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'stomp'
 
@@ -8,7 +9,7 @@ stomp_hash = {
       login: 'guest',
       passcode: 'guest',
       host: 'localhost',
-      port: 61613
+      port: 61_613
     }
   ],
   reliable: true,
@@ -35,7 +36,7 @@ stomp_hash = {
   start_timeout: 0,
   sslctx_newparm: nil,
   ssl_post_conn_check: true,
-  nto_cmd_read: true,
+  nto_cmd_read: true
 }
 
 def do_connect
@@ -53,13 +54,13 @@ def do_connect
     @client = @socket
   else
     @client = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
-  
+
     puts "Connecting to #{ENV['FAZ_MUD_HOST']} on port #{ENV['FAZ_MUD_PORT']}"
     @client.connect Socket.pack_sockaddr_in(ENV['FAZ_MUD_PORT'], ENV['FAZ_MUD_HOST'])
-    puts "Connected!"
-  
+    puts 'Connected!'
+
     unless ENV['FAZ_PASS']
-      puts "ERROR: Must have a variable set for FAZ_PASS in the local env"
+      puts 'ERROR: Must have a variable set for FAZ_PASS in the local env'
       exit 1
     end
   end
@@ -69,45 +70,54 @@ end
 
 ### Start thread to read messages off the bus and act accordingly.
 Thread.new do
-  begin
-    stomp = Stomp::Client.new(stomp_hash)
-    puts "Connected to STOMP broker for outbound messages"
-    stomp.subscribe("/queue/send_to_#{ENV['FAZ_QUEUE_NAME']}") do |msg|
-      body = msg.body
-      puts "Sending to MUD: #{body}"
-      @client.puts body
-    end
-  rescue Interrupt => e
-    stomp.close
-    exit(1)
+  stomp = Stomp::Client.new(stomp_hash)
+  puts 'Connected to STOMP broker for outbound messages'
+  stomp.subscribe("/queue/send_to_#{ENV['FAZ_QUEUE_NAME']}") do |msg|
+    puts "DEBUG: Received message object: #{msg.class}"
+    body = msg.body
+    puts "DEBUG: Extracted body: #{body.inspect}"
+    puts "Sending to MUD: #{body}"
+    @client.puts body
   end
+rescue Interrupt
+  stomp.close
+  exit(1)
 end
 
-
 main_stomp = Stomp::Client.new(stomp_hash)
-puts "Connected to STOMP broker for inbound messages"
+puts 'Connected to STOMP broker for inbound messages'
 
 while 1 == 1
   do_connect
+  @keepalive_failed = false
+  keepalive_thread = nil
+
   if ENV['FAZ_KEEPALIVE']
-    Thread.new do 
-      while 1 == 1
-        sleep ENV['FAZ_KEEPALIVE']
+    keepalive_thread = Thread.new do
+      until @keepalive_failed
+        sleep ENV['FAZ_KEEPALIVE'].to_i
         begin
           @client.puts '@@'
-        rescue => e
-          puts "Reconnecting!!"
-          do_connect
+        rescue StandardError => e
+          puts "Keepalive failed: #{e}"
+          @keepalive_failed = true
+          begin
+            @client.close
+          rescue StandardError
+            nil
+          end
+          break
         end
       end
     end
   end
+
   begin
-    @client.puts "say HELLO"
-    while line = @client.gets
+    @client.puts 'say HELLO'
+    while !@keepalive_failed && (line = @client.gets)
       puts " GOT LINE: #{line}"
-      if line =~ /^\[[a-zA-Z0-9]/ and line.split(' ').count > 1
-        if line =~ /page/ or line =~ /saypose/ or line =~ /, "/
+      if line =~ /^\[[a-zA-Z0-9]/ && (line.split(' ').count > 1)
+        if line =~ /page/ || line =~ /saypose/ || line =~ /, "/
           puts "Publishing to queue: #{ENV['FAZ_QUEUE_NAME']}_received - #{line.strip}"
           main_stomp.publish("/queue/#{ENV['FAZ_QUEUE_NAME']}_received", line)
         end
@@ -115,9 +125,18 @@ while 1 == 1
         puts " Dunno what to do with line: #{line}"
       end
     end
-  rescue => e
-    puts " Got error: #{e} sleeping for 10..."
-    sleep(90)
+  rescue StandardError => e
+    puts " Got error: #{e}"
+  ensure
+    @keepalive_failed = true
+    keepalive_thread&.kill
+    begin
+      @client.close
+    rescue StandardError
+      nil
+    end
+    puts 'Connection closed, reconnecting in 10 seconds...'
+    sleep(10)
   end
 end
 
